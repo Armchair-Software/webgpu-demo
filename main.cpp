a0195c99f8337eeffe8f1c1c1b877be732ec4050
SlowRiot <slowriot@voxelstorm.com>
2024-11-14 15:53:47 +0000

    get rid of unused boost no exception code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
-#include <boost/throw_exception.hpp>
 #include <emscripten.h>
 #include "logstorm/logstorm.h"
 #include "render/webgpu_renderer.h"
-
-#ifdef BOOST_NO_EXCEPTIONS
-void boost::throw_exception(std::exception const & e) {
-  /// Custom exception replacement function when boost exceptions are disabled
-  std::cerr << "ERROR: Boost would have thrown exception: " << e.what() << std::endl;
-  abort();
-  std::unreachable();
-}
-#endif // BOOST_NO_EXCEPTIONS
 
 class game_manager {
   logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::console>()}; // logging system
