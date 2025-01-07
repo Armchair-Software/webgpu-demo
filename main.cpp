7258b5e22b88e613093a582b824a94ab295b9f20
SlowRiot <slowriot@voxelstorm.com>
2025-01-07 20:06:21 +0000

    use new emscripten sink for logstorm

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "render/webgpu_renderer.h"
 
 class game_manager {
-  logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::console>()}; // logging system
+  logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::emscripten_out>()}; // logging system
   render::webgpu_renderer renderer{logger, [&]{loop_main();}};                  // WebGPU rendering system
 
   void loop_main();
